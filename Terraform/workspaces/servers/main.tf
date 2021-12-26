e9855637c63c597c9b4c61e22d9d711d3d14d6fa
Ofir Gan <offirgan@gmail.com>
2021-12-26 20:27:05 +0200

    fix workspace

--- Terraform/workspaces/servers/main.tf
+++ Terraform/workspaces/servers/main.tf
@@ -20,19 +20,20 @@ module "servers" {
   source  = "app.terraform.io/Kandula-OpsSchool-OfirGan/servers/aws"
   version = "1.0.0"
 
-
-
-
   vpc_id               = data.terraform_remote_state.vpc.outputs.vpc_id
   available_zone_names = data.terraform_remote_state.vpc.outputs.available_zone_names
-  s3_logs_bucket_name  = var.s3_logs_bucket_name
-  s3_logs_folder       = var.s3_logs_folder
   public_subnets_ids   = data.terraform_remote_state.vpc.outputs.public_subnets_ids
   private_subnets_ids  = data.terraform_remote_state.vpc.outputs.private_subnets_ids
-  instance_count       = var.instance_count
-  instance_type_web    = var.instance_type
-  instance_type_db     = var.instance_type
-  key_name             = aws_ec2_key_pair_name
-  purpose_tag          = var.purpose_tag
-  owner_tag            = var.owner_tag
+
+  s3_logs_bucket_name = var.s3_logs_bucket_name
+  elb_account_id      = var.elb_account_id
+
+  instance_type        = var.instance_type
+  consul_servers_count = var.consul_servers_count
+  jenkins_nodes_count  = var.jenkins_nodes_count
+
+  project_name = var.project_name
+  owner_name   = var.owner_name
+
+  aws_server_key_name = aws_key_pair.server_key.key_name
 }
--- Terraform/workspaces/servers/outputs.tf
+++ Terraform/workspaces/servers/outputs.tf
@@ -1,1 +1,44 @@
 
+###################################################################################
+# OUTPUT
+###################################################################################
+
+output "bastion_server_public_ip" {
+  description = "Bastion host Public IP"
+  value       = module.servers.bastion_server_public_ip
+}
+
+output "bastion_server_private_ip" {
+  description = "Bastion host Private IP"
+  value       = module.servers.bastion_server_private_ip
+}
+
+output "consul_servers_private_ips" {
+  description = "Consul Servers Private IP's"
+  value       = module.servers.jenkins_server_private_ip
+}
+
+output "jenkins_server_private_ip" {
+  description = "Jenkins server Private IP"
+  value       = module.servers.jenkins_server_private_ip
+}
+
+output "jenkins_nodes_private_ip" {
+  description = "Private IP's of the Jenkins nodes"
+  value       = module.servers.jenkins_nodes_private_ip
+}
+
+output "ansible_server_private_ip" {
+  description = "Ansible Server Private IP"
+  value       = module.servers.ansible_server_private_ip
+}
+
+output "consul_alb_public_dns" {
+  description = "Consul ALB Public DNS name"
+  value       = module.servers.consul_alb_public_dns
+}
+
+output "jenkins_alb_public_dns" {
+  description = "Jenkins ALB Public DNS name"
+  value       = module.servers.jenkins_alb_public_dns
+}
