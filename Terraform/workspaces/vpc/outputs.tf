25dcb52035f350da3a0ff6dbe8205269de0759fc
Ofir Gan <offirgan@gmail.com>
2021-12-27 02:15:09 +0200

    add missing output

--- Terraform/workspaces/vpc/outputs.tf
+++ Terraform/workspaces/vpc/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnets_ids" {
 output "private_subnets_ids" {
   value = module.vpc.private_subnets_ids
 }
+
+output "cluster_name" {
+  description = "Kubernetes Cluster name"
+  value       = module.vpc.cluster_name
+}
