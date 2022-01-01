e27b700bce185d4858c87b319e66132a0ba5337e
Ofir Gan <offirgan@gmail.com>
2022-01-01 22:53:46 +0200

    Clean Main Branch

--- Terraform/modules/terraform-tfe-servers-workspace/outputs.tf
+++
@@ -1,8 +0,0 @@
-###################################################################################
-# OUTPUT
-###################################################################################
-
-output "workspace_id" {
-  description = "Servers Workspace ID"
-  value       = resource.tfe_workspace.servers_workspace.id
-}
