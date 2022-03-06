88e516c304a57861f8b98cf7502ef528c33bb893
Ofir Gan <offirgan@gmail.com>
2022-03-06 10:24:53 +0200

    Add Monitor Security Groups to K8s

--- Terraform/workspaces/kubernetes/security-groups.tf
+++ Terraform/workspaces/kubernetes/security-groups.tf
@@ -15,3 +15,39 @@ resource "aws_security_group" "all_worker_mgmt" {
     ]
   }
 }
+
+resource "aws_security_group" "node_exporter_k8s_sg" {
+  name        = "node_exporter_k8s_sg"
+  description = " Node Exporter Security group"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port = 9100
+    to_port   = 9100
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "kube_state_metrics_k8s_sg" {
+  name        = "kube_state_metrics_k8s_sg"
+  description = "Kube State Metrics Security group"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
