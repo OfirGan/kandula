4d622f0218098f0d06bec86e8ec02b8896a8594e
Ofir Gan <offirgan@gmail.com>
2022-03-06 23:09:20 +0200

    disable terraform k8s consul creation

--- Terraform/workspaces/kubernetes/k8s_consul.tf
+++ Terraform/workspaces/kubernetes/k8s_consul.tf
@@ -1,48 +1,48 @@
-resource "kubernetes_secret_v1" "consul_secrets" {
-  metadata {
-    name = "${module.eks.cluster_id}-hcp"
-  }
+# resource "kubernetes_secret_v1" "consul_secrets" {
+#   metadata {
+#     name = "${module.eks.cluster_id}-hcp"
+#   }
 
-  data = {
-    gossipEncryptionKey = "uDBV4e+LbFW3019YKPxIrg=="
-  }
+#   data = {
+#     gossipEncryptionKey = "uDBV4e+LbFW3019YKPxIrg=="
+#   }
 
-  type = "Opaque"
-}
+#   type = "Opaque"
+# }
 
-resource "helm_release" "consul" {
-  name       = "consul"
-  repository = "https://helm.releases.hashicorp.com"
-  version    = var.consul_chart_version
-  chart      = "consul"
+# resource "helm_release" "consul" {
+#   name       = "consul"
+#   repository = "https://helm.releases.hashicorp.com"
+#   version    = var.consul_chart_version
+#   chart      = "consul"
 
-  values = [
-    templatefile("k8s_consul.tpl", {
-      datacenter       = "kandula-dc"
-      consul_hosts     = jsonencode(var.consul_hosts)
-      cluster_id       = module.eks.cluster_id
-      k8s_api_endpoint = module.eks.cluster_endpoint
-      consul_version   = substr(var.consul_version, 1, -1)
-    })
-  ]
+#   values = [
+#     templatefile("k8s_consul.tpl", {
+#       datacenter       = "kandula-dc"
+#       consul_hosts     = jsonencode(var.consul_hosts)
+#       cluster_id       = module.eks.cluster_id
+#       k8s_api_endpoint = module.eks.cluster_endpoint
+#       consul_version   = substr(var.consul_version, 1, -1)
+#     })
+#   ]
 
-  # Helm installation relies on the Kuberenetes secret being
-  # available.
-  depends_on = [kubernetes_secret_v1.consul_secrets]
-}
+#   # Helm installation relies on the Kuberenetes secret being
+#   # available.
+#   depends_on = [kubernetes_secret_v1.consul_secrets]
+# }
 
-variable "consul_hosts" {
-  default = "provider=aws tag_key=service_role tag_value=service_discovery region=us-east-1"
-}
+# variable "consul_hosts" {
+#   default = "provider=aws tag_key=service_role tag_value=service_discovery region=us-east-1"
+# }
 
-variable "consul_version" {
-  type        = string
-  description = "Consul version"
-  default     = "1.11.1"
-}
+# variable "consul_version" {
+#   type        = string
+#   description = "Consul version"
+#   default     = "1.11.1"
+# }
 
-variable "consul_chart_version" {
-  type        = string
-  description = "The Consul Helm chart version to use"
-  default     = "0.41.1"
-}
+# variable "consul_chart_version" {
+#   type        = string
+#   description = "The Consul Helm chart version to use"
+#   default     = "0.41.1"
+# }
